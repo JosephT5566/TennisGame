fe12bb53510aa81b27d3edf3c6bd360e9da84c93
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-06-19 21:01:09 +0800

    Adding unit test items

--- TennisGameTests/ScoreboardTests.cs
+++ TennisGameTests/ScoreboardTests.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisGame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TennisGameTests
+namespace TennisGame.Tests
 {
-    class ScoreboardTests
+    [TestClass()]
+    public class ScoreboardTests
     {
+        [TestMethod()]
+        public void getTermTest_loveAll()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            scoreboard.Point1 = "0";
+            scoreboard.Point2 = "0";
+            string term = scoreboard.getTerm();
+            Assert.AreEqual("Love All", term);
+        }
+        [TestMethod()]
+        public void getTermTest_fifteenLove()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            scoreboard.Point1 = "1";
+            scoreboard.Point2 = "0";
+            string term = scoreboard.getTerm();
+            Assert.AreEqual("Fifteen Love", term);
+        }
     }
 }
